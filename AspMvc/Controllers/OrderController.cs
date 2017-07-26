cb0a655c50a19640fd305fde1d3a89c3aed77aeb
r12asp <r12asp@hotmail.com>
2017-07-27 07:34:01 +1000

    Rename

---
+++ AspMvc/Controllers/OrderController.cs
@@ -0,0 +1,41 @@
+﻿using DomainEntityOrder;
+using DomainEntityOrder.DomainService;
+using DomainModelOrder.Constant;
+using DomainModelOrder.ViewModel;
+using DomainServiceOrder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebClientOrder.Controllers
+{
+    public class OrderController : Controller
+    {
+        private IOrderService so;
+        public OrderController(IOrderService so)
+        {
+            this.so = so;
+        }
+        [HttpGet]
+        public ActionResult Index(OrderSearchViewModel searchModel,string page)
+        {
+            var pp = new PaginationViewModel
+            {
+                CurrntPage = (string.IsNullOrEmpty(page) || int.Parse(page)==0) ? 1 : int.Parse(page),
+                PageSize = GlobalConsts.PageSize,
+            };
+
+            OrderViewPageViewModel pageModel = new OrderViewPageViewModel
+            {
+                Orders = so.GetOrderViewsAll(searchModel, pp ),
+                OrderSearch = searchModel,
+                Pagination =pp,
+            };
+            return View(pageModel);
+        }
+
+
+    }
+}
